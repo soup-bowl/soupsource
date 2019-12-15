5b375583c0819824f33b0dcbb6fdb8629a7bf70f
soup-bowl <code@soupbowl.io>
2019-12-15 00:49:00 +0000

    CSS organisation changes.

--- src/themes/basis/header.php
+++ src/themes/basis/header.php
@@ -1,8 +1,9 @@
 <head>
 	<title>Page - <?php echo SITE_TITLE;?></title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="/themes/basis/main.css">
 </head>
-<body style="padding-top: 5rem;">
+<body>
 	<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
 		<a class="navbar-brand" href="/"><?php echo SITE_TITLE;?></a>
 		<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#savvyNavvy" aria-controls="savvyNavvy" aria-expanded="false" aria-label="Toggle navigation">
